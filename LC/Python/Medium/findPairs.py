0b59cf3878a2f7a76bf04b6a44237cb2e5ee4268
Mousa Zourob <mousazourob@gmail.com>
2021-12-31 14:29:12 -0500

    Find pairs

---
+++ LC/Python/Medium/findPairs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findPairs(self, nums: List[int], k: int) -> int:
+        ans = 0
+        n = Counter(nums)
+        
+        for key in n:
+            if k == 0:
+                if n[key] > 1:
+                    ans += 1
+            else:
+                if key - k in n:
+                    ans += 1
+                    
+        return ans
