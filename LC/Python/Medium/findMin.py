b97f17eee72c784cf46cdcb86e627e99b6c21e09
Mousa Zourob <mousazourob@gmail.com>
2022-01-08 12:08:12 -0500

    Find min

---
+++ LC/Python/Medium/findMin.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        l = 0
+        r = len(nums)-1
+
+        while l < r:
+            if (nums[l] < nums[r]):
+                return nums[l]
+                
+            m = (r + l) // 2
+
+            if (nums[m] >= nums[l]):
+                l = m+1
+            else:
+                r = m
+
+        return nums[l]
