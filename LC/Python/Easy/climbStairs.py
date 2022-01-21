4ad436305c86cc3cbc6908d094f9e1892d5f0983
Mousa Zourob <mousazourob@gmail.com>
2022-01-21 13:17:21 -0500

    Climb stairs

---
+++ LC/Python/Easy/climbStairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 0: return 0
+        
+        a = 0
+        b = 1
+        
+        for i in range(n):
+            c = a + b
+            a = b
+            b = c
+        
+        return c
