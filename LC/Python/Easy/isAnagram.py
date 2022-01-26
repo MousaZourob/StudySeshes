695580966c465c11bc7201dd9117fbc0336143b0
Mousa Zourob <mousazourob@gmail.com>
2022-01-26 18:29:39 -0500

    Is anagram

--- LC/Python/Easy/isAnagram.py
+++ LC/Python/Easy/isAnagram.py
@@ -1,3 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+        s_table, t_table = {}, {}
+        
+        if len(s) != len(t): return False
+        
+        for i in range(len(s)):
+            s_table[s[i]] = 1 + s_table.get(s[i], 0)
+            t_table[t[i]] = 1 + t_table.get(t[i], 0)
+
+        return s_table == t_table
