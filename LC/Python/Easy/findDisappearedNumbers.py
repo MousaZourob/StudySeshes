4acec802376ec12ab6ba3ec2e0778642358ce5ec
Mousa Zourob <mousazourob@gmail.com>
2022-02-14 20:47:39 -0500

    Find Disappeared Numbers

---
+++ LC/Python/Easy/findDisappearedNumbers.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        start = 0
+        ans = []
+        
+        while start < len(nums):
+            j = nums[start] - 1
+            if j < len(nums) and nums[start] != nums[j]:
+                nums[start], nums[j] = nums[j], nums[start]
+            else:
+                start += 1
+        
+        for i in range(len(nums)):
+            if i+1 != nums[i]:
+                ans.append(i+1)
+        
+        return ans
