2669652cf6c519bc43b4345dfadf13009f6ea214
Mousa Zourob <mousazourob@gmail.com>
2022-02-16 16:02:57 -0500

    Find Duplicates

--- LC/Python/Medium/findDuplicates.py
+++ LC/Python/Medium/findDuplicates.py
@@ -1,11 +1,17 @@
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
+        start = 0
         ans = []
         
-        for i in range(len(nums)):
-            if nums[abs(nums[i])-1] < 0:
-                ans.append(abs(nums[i]))
+        while start < len(nums):
+            j = nums[start] - 1
+            if j < len(nums) and nums[start] != nums[j]:
+                nums[start], nums[j] = nums[j], nums[start]
             else:
-                nums[abs(nums[i])-1] *= -1 
-                
+                start += 1
+        
+        for i in range(len(nums)):
+            if i+1 != nums[i]:
+                ans.append(nums[i])
+        
         return ans
