c6aa64adf8d50b88cc95a7e8395665959cd076a9
Mousa Zourob <mousazourob@gmail.com>
2022-02-22 20:22:14 -0500

    Level Order Bottom

---
+++ LC/Python/Medium/levelOrderBottom.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        ans = []
+        q = collections.deque()
+        q.append(root)
+        
+        while q:
+            levelSize = len(q)
+            curr_level = []
+            
+            for i in range(levelSize):
+                curr = q.popleft()
+                if curr:
+                    curr_level.append(curr.val)
+                    if curr.left:
+                        q.append(curr.left)
+                    if curr.right:
+                        q.append(curr.right)
+                        
+            if curr_level:
+                ans = [curr_level] + ans
+        
+        return ans
