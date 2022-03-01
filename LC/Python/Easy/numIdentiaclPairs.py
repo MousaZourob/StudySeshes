e555925cc23a6dd8fca37b4db09471038244ae43
Mousa Zourob <mousazourob@gmail.com>
2022-03-01 14:02:27 -0500

    Num Identical Pairs

---
+++ LC/Python/Easy/numIdentiaclPairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        ans = 0
+        count = [0] * 101
+        
+        for num in nums:
+            ans += count[num]
+            count[num] += 1
+        
+        return ans
