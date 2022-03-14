198c7daf7fa4cd7b47bc1ab4331326d4bbb4c0dd
Mousa Zourob <mousazourob@gmail.com>
2022-03-14 15:49:37 -0400

    Letter Permuate

---
+++ LC/Python/Medium/letterCasePermutate.py
@@ -0,0 +1,16 @@
+class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
+        ans = [s]
+        
+        for i in range(len(s)):
+            if s[i].isalpha():
+                for j in range(len(ans)):
+                    chars = list(ans[j])
+                    
+                    if chars[i] == chars[i].lower():
+                        chars[i] = chars[i].upper()
+                    else:
+                        chars[i] = chars[i].lower()
+                    ans.append(''.join(chars))
+                    
+        return ans
