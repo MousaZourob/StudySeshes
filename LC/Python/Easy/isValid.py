a4b2c7384ee11f11108dc0bfe1cabe339ab2ae90
Mousa Zourob <mousazourob@gmail.com>
2022-03-14 14:06:26 -0400

    Is Valid

--- LC/Python/Easy/isValid.py
+++ LC/Python/Easy/isValid.py
@@ -1,5 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        while "()" in s or "{}" in s or '[]' in s:
-            s = s.replace("()", "").replace('{}', "").replace('[]', "")
-        return s == ''
+        valid_combs = {
+            ']' : '[',
+            ')' : '(',
+            '}' : '{'
+        }
+        stack = []
+        
+        for c in s:
+            if c in valid_combs:
+                if stack and stack[-1] == valid_combs[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        
+        return True if not stack else False
