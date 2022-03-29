4ccb08127a06bcc0a9ec9e96525344112ed9b596
Mousa Zourob <mousazourob@gmail.com>
2022-03-29 15:22:40 -0400

    Kth Smallest

--- LC/Python/Medium/kthSmallest.py
+++ LC/Python/Medium/kthSmallest.py
@@ -2,11 +2,20 @@ class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
         heap = []
         heapify(heap)
-        
-        for r in matrix:
-            for n in r:
-                heappush(heap, -n)
-                if len(heap) > k:
-                    heappop(heap)
 
-        return -heap[0]
+        for l in matrix:
+            heappush(heap, (l[0], 0, l))
+        
+        num = 0
+        
+        while heap:
+            num, index, lst = heappop(heap)
+            
+            k -= 1
+            if k == 0: 
+                break
+                
+            if len(lst) > index + 1:
+                heappush(heap, (lst[index+1], index+1, lst))
+        
+        return num
